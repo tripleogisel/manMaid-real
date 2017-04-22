c4478a2853422e145b4cb03a7c3ff2545a37f793
WIll Gisel <tripleogisel@WIlls-MacBook-Pro-2.local>
2017-04-22 13:55:09 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log('main.js is loaded');
+
+$('.stickyButton').on('click', function(){
+	$('.formStart').focus();
+})
+
+// $(window).on('scroll', function (){
+// 	$('.site').removeClass('siteLoad');
+// })
+
+console.log('main.js is finished');
